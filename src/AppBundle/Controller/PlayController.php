dc886ff33a35ee8d002a6885f2d9424abdfbcba4
steve <smartin@essentialpos.com>
2016-04-14 13:59:10 -0500

    clean up code

--- src/AppBundle/Controller/PlayController.php
+++ src/AppBundle/Controller/PlayController.php
@@ -13,101 +13,106 @@ use AppBundle\Entity\Games;
 
 class PlayController extends Controller
 {
-    	/**
-     	* @Route("/start")
-     	*/
-    	public function startAction()
-    	{
-		return $this->render('play/start.html.twig', []);
-    	}
+    /**
+     * @Route("/start")
+     */
+    public function startAction()
+    {
+        return $this->render('play/start.html.twig', []);
+    }
 
-	/**
-	* @Route("/winner")
-	* @Method({"POST"})
-	*/
-	public function winner(){
-		$myChoice = @$_POST["myChoice"];
-		$computerChoice = @$_POST["computerChoice"];
+    /**
+     * @Route("/winner")
+     * @Method({"POST"})
+     */
+    public function winner()
+    {
+        $myChoice = @$_POST["myChoice"];
+        $computerChoice = @$_POST["computerChoice"];
 
-		$status = -1;
+        $status = -1;
 
-		if($myChoice == $computerChoice){
-			$status = -1;
-		}
-		else{
-			$em = $this->getDoctrine()->getManager();
+        if($myChoice == $computerChoice){
+            $status = -1;
+        }
+        else{
+            $em = $this->getDoctrine()->getManager();
 
-			$choice = $em->getRepository('AppBundle:Winner')
-                        	->findOneBy(array('choiceId' => $myChoice));
+            $choice = $em->getRepository('AppBundle:Winner')
+                ->findOneBy(array('choiceId' => $myChoice));
 
-			$tmp = explode(',', $choice->getChoiceLose());
+            $tmp = explode(',', $choice->getChoiceLose());
 
-			if(in_array($computerChoice, $tmp)){
-				$status = 0;
-			}
-			else{
-				$status = 1;
-			}
-		}
+            if(in_array($computerChoice, $tmp)){
+                $status = 0;
+            }
+            else{
+                $status = 1;
+            }
+        }
 
-		//save game to database
-		$game = new Games();
-        	$game->setSession(session_id());
-		$game->setWinner($status);
-		$game->setUserChoice($myChoice);
-		$game->setComputerChoice($computerChoice);
-        	$em = $this->getDoctrine()->getManager();
-        	$em->persist($game);
-        	$em->flush();
+        //save game to database
+        $game = new Games();
+        $game->setSession(session_id());
+        $game->setWinner($status);
+        $game->setUserChoice($myChoice);
+        $game->setComputerChoice($computerChoice);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($game);
+        $em->flush();
 
-		return new Response($status);
-	}
+        return new Response($status);
+    }
 
-	/**
-	* @Route("/getScore")
-	*/
-	public function getScore(){
-		$em = $this->getDoctrine()->getManager();
+    /**
+     * @Route("/getScore")
+     */
+    public function getScore()
+    {
+        $em = $this->getDoctrine()->getManager();
         	
-		$score = $em->getRepository('AppBundle:Games')
-            		->getScoreBySession();
+        $score = $em->getRepository('AppBundle:Games')
+            ->getScoreBySession();
 		
-		return new JsonResponse($score);
-	}
+        return new JsonResponse($score);
+    }
 
-	/**
-	* @Route("/getUserChoices")
-	*/
-	public function getUserChoices(){
-		$em = $this->getDoctrine()->getManager();
+    /**
+     * @Route("/getUserChoices")
+     */
+    public function getUserChoices()
+    {
+        $em = $this->getDoctrine()->getManager();
 
-                $score = $em->getRepository('AppBundle:Games')
-                        ->getChoicesByUser();
+        $score = $em->getRepository('AppBundle:Games')
+            ->getChoicesByUser();
 
-                return new JsonResponse($score);
-	}
+        return new JsonResponse($score);
+    }
 
-	/**
-	* @Route("/getChoices")
-	*/
-	public function getChoices(){
-		$em = $this->getDoctrine()->getManager();
-        	
-		$choices = $em->getRepository('AppBundle:Choices')
-            		->findAll( );
+    /**
+     * @Route("/getChoices")
+     */
+    public function getChoices()
+    {
+        $em = $this->getDoctrine()->getManager();
 
-		$serializer = new Serializer(array(new GetSetMethodNormalizer()), array('json' => new JsonEncoder()));
-		$json = json_decode($serializer->serialize($choices, 'json'));
+        $choices = $em->getRepository('AppBundle:Choices')
+            ->findAll( );
 
-		return new JsonResponse($json);
-	}
+        $serializer = new Serializer(array(new GetSetMethodNormalizer()), array('json' => new JsonEncoder()));
+        $json = json_decode($serializer->serialize($choices, 'json'));
 
-	/**
-	* @Route("/computerPick")
-	*/
-	public function computerPick(){
-		sleep(1);
+        return new JsonResponse($json);
+    }
 
-		return new Response(rand(1, 5));
-	}
+    /**
+     * @Route("/computerPick")
+     */
+    public function computerPick()
+    {
+        sleep(1);
+
+        return new Response(rand(1, 5));
+    }
 }
--- web/js/start.js
+++ web/js/start.js
@@ -2,151 +2,174 @@ var myChoice = 0;
 var computerChoice = 0;
 
 $( document ).ready(function() {
-	getChoices();
+    getChoices();
 });
 
-$('#myChoices').on('click', '.choice', function() {
-	$('.choice').attr('disabled','disabled');
-	$('#' + $(this).attr('id')).css('background-color','yellow');
-	myChoice = $(this).attr('id');
-	$('#computerChoices').show();
+$('#myChoices').on('click', '.choice', function() 
+{
+    $('.choice').attr('disabled','disabled');
+    $('#' + $(this).attr('id')).css('background-color','yellow');
+    myChoice = $(this).attr('id');
+    $('#computerChoices').show();
 
-	computerPick();
+    computerPick();
 });
 
-function showResults(){
+function showResults()
+{
 	$.ajax({
-                type: 'POST',
-                url: 'winner',
-                data: 'myChoice=' + myChoice + '&computerChoice=' + computerChoice,
-                success: function( r ) {
-			if(r == '-1'){
-				$('#result').html('<h1>Draw</h1>');
-			}
-			else if(r == '1'){
-				$('#result').html('<h1>Win</h1>');
-			}
-			else{
-				$('#result').html('<h1>Lose</h1>');
-			}
-                },
-                error: function ( r ) {
-                        alert(r);
-                }
-        });
+        type: 'POST',
+        url: 'winner',
+        data: 'myChoice=' + myChoice + '&computerChoice=' + computerChoice,
+        success: function( r ) 
+        {
+            if(r == '-1'){
+                $('#result').html('<h1>Draw</h1>');
+            }
+            else if(r == '1'){
+                $('#result').html('<h1>Win</h1>');
+            }
+            else{
+                $('#result').html('<h1>Lose</h1>');
+            }
+        },
+        error: function ( r ) 
+        {
+            alert(r);
+        }
+    });
 	
-	setTimeout(function(){ 
-		getChoices(); 
-	}, 3000);
+    setTimeout(function()
+    { 
+        getChoices(); 
+    }, 3000);
 }
 
-function computerPick(){
-	$.ajax({
-                type: 'GET',
-                url: 'computerPick',
-                data: '',
-                success: function( r ) {
-			$('.choice').each(function(i, obj) {
-				if($(this).attr('id') == ('c' + r)){
-					computerChoice = r;
-					$('#c' + r).css('background-color','yellow');
-				}
-			});
-
-			showResults();	
-                },
-                error: function ( r ) {
-                        alert(r);
+function computerPick()
+{
+    $.ajax({
+        type: 'GET',
+        url: 'computerPick',
+        data: '',
+        success: function( r ) 
+        {
+            $('.choice').each(function(i, obj) 
+            {
+                if($(this).attr('id') == ('c' + r)){
+                    computerChoice = r;
+                    $('#c' + r).css('background-color','yellow');
                 }
-        });
+            });
+
+            showResults();	
+        },
+        error: function ( r ) 
+        {
+            alert(r);
+        }
+    });
 }
 
-function getScore(){
-	$.ajax({
-                type: 'GET',
-                url: 'getScore',
-                data: '',
-                success: function( r ) {
-			var id;
-			for(id in r){
-				$('#score' + r[id]['winner']).html(r[id]['score']);
-			}
-                },
-                error: function ( r ) {
-                        alert(r);
+function getScore()
+{
+    $.ajax({
+        type: 'GET',
+        url: 'getScore',
+        data: '',
+        success: function( r ) 
+        {
+            var id;
+            for(id in r){
+                $('#score' + r[id]['winner']).html(r[id]['score']);
+            }
+        },
+        error: function ( r ) 
+        {
+            alert(r);
+        }
+    });
+}
+
+function getUserChoices()
+{
+    $.ajax({
+        type: 'GET',
+        url: 'getUserChoices',
+        data: '',
+        success: function( r ) 
+        {
+            var id;
+            var data = [];
+            for(id in r){
+                if(!data[r[id]['choiceName']]){
+                    data[r[id]['choiceName']] = [];
+                }				
+
+                if(r[id]['user'] == 1){
+                    data[r[id]['choiceName']]['You'] = parseInt(r[id]['num'])<1? '0':parseInt(r[id]['num']);
                 }
-        });
-}
-
-function getUserChoices(){
-	$.ajax({
-                type: 'GET',
-                url: 'getUserChoices',
-                data: '',
-                success: function( r ) {
-			var id;
-			var data = [];
-			for(id in r){
-				if(!data[r[id]['choiceName']]){
-					data[r[id]['choiceName']] = [];
-				}				
-
-				if(r[id]['user'] == 1){
-					data[r[id]['choiceName']]['You'] = parseInt(r[id]['num'])<1? '0':parseInt(r[id]['num']);
-				}
-				else{
-					data[r[id]['choiceName']]['Computer'] = parseInt(r[id]['num'])<1? '0':parseInt(r[id]['num']);
-				}
-			}
-
-			var html = '<tr><td>Choice</td><td>You</td><td>Computer</td></tr>';
-		
-			for(id in data){
-				if(!data[id]['You']){
-                                        data[id]['You'] = 0;
-                                }
-
-				if(!data[id]['Computer']){
-					data[id]['Computer'] = 0;
-				}
-				html += '<tr><td>' + id + '</td><td>' + data[id]['You'] + '</td><td>' + data[id]['Computer'] + '</td></tr>';
-			}
-			$('#choiceTotals').html(html);
-                },
-                error: function ( r ) {
-                        alert(r);
+                else{
+                    data[r[id]['choiceName']]['Computer'] = parseInt(r[id]['num'])<1? '0':parseInt(r[id]['num']);
                 }
-        });
+            }
+
+            var html = '<tr><td>Choice</td><td>You</td><td>Computer</td></tr>';
+
+            for(id in data){
+                if(!data[id]['You']){
+                    data[id]['You'] = 0;
+                }
+
+                if(!data[id]['Computer']){
+                    data[id]['Computer'] = 0;
+                }
+                
+                html += '<tr><td>' + id + '</td><td>' + data[id]['You'] 
+                    + '</td><td>' + data[id]['Computer'] + '</td></tr>';
+            }
+            
+            $('#choiceTotals').html(html);
+        },
+        error: function ( r ) 
+        {
+            alert(r);
+        }
+    });
 }
 
-function getChoices(){
-	myChoice = 0;
-	computerChoice = 0;
-	$('#result').html('');
+function getChoices()
+{
+    myChoice = 0;
+    computerChoice = 0;
+    $('#result').html('');
 
-	$.ajax({
-  		type: 'GET',
-  		url: 'getChoices',
-  		data: '',
-		success: function( r ) {
-			var id;
-			var myData = '';
-			var computerData = '';
+    $.ajax({
+        type: 'GET',
+        url: 'getChoices',
+        data: '',
+        success: function( r ) 
+        {
+            var id;
+            var myData = '';
+            var computerData = '';
 
-			//print button choices
-			for(id in r){
-				myData += '<button class="choice" id="' + r[id]['id'] + '">' + r[id]['name'] + '</button>';
-				computerData += '<button class="choice" id="c' + r[id]['id'] + '" disabled="disabled">' + r[id]['name'] + '</button>';
-			}
+            //print button choices
+            for(id in r){
+                myData += '<button class="choice" id="' + r[id]['id'] + '">' 
+                    + r[id]['name'] + '</button>';
+                computerData += '<button class="choice" id="c' + r[id]['id'] 
+                    + '" disabled="disabled">' + r[id]['name'] + '</button>';
+            }
 
-			$('#myChoices').html(myData);
-			$('#computerChoices').html(computerData);
+            $('#myChoices').html(myData);
+            $('#computerChoices').html(computerData);
 
-			getScore();
-			getUserChoices();
-		},
-		error: function ( r ) {
-			alert(r);
-		}
-	});	
+            getScore();
+            getUserChoices();
+        },
+        error: function ( r ) 
+        {
+            alert(r);
+        }
+    });	
 }
